a5cd5a28f9825de0a0774585098d9eb4b4242731
Paulo Borges <paulohtb@hotmail.com>
2023-07-07 14:12:47 -0300

    engine: create eol version banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,6 +117,9 @@ module.exports = async () => {
               current: {
                 label: '23.1',
               },
+              "21.11": {
+                banner:'unmaintained'
+              },
               "23.2": {
                 label: '23.2 Beta',
                 banner: 'unreleased',
